97115fd9aec4cf0baef565144c4f98d7eb1ef5c6
ayouba <ayoubadoumbia041@gmail.com>
2025-03-04 08:57:52 +0100

    add db connexion

--- db.client.js
+++ db.client.js
@@ -1,9 +1,20 @@
 const { Sequelize } = require('sequelize')
 
+const host = process.env.DB_HOST
+const port = process.env.DB_PORT 
+const database = process.env.DB_NAME 
+const username = process.env.DB_USERNAME 
+const password = process.env.DB_PASSWORD 
+
 // database
 const sequelize = new Sequelize(
-  'postgres://fakeurl', // TODO
+  database,
+  username,
+  password,
   {
+    host: host,
+    port: port,
+    dialect: 'postgres',
     dialectOptions: {
       ssl: {
         require: true,
